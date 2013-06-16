b01ddfaf55ba1dc0a0cdadf89cebeac29d765c1c
Russell <russell@russelldunphy.com>
2013-06-16 20:25:48 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrydiaryCom::Application.config.secret_key_base = '39c78dc768b0b12c7e6610ef388d45d39b3f31b05d4ba88c840b0aa74e3e864870dd637b6cd80cd580b3106065ba2b9b0d1e4d9515323f0f6bc639da3394131e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DrydiaryCom::Application.config.session_store :cookie_store, key: '_drydiary_com_session'
