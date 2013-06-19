355d33a7623e323825a2d5ec461ef5635dc2c15e
Russell <russell@russelldunphy.com>
2013-06-19 07:56:43 +0100

    Grey icon if no entry has been saved

--- app/helpers/dry_helper.rb
+++ app/helpers/dry_helper.rb
@@ -5,4 +5,8 @@ module DryHelper
     else new_day_path(day, date: day.date) end
   end
 
+  def tick_icon(day)
+    if day.persisted? then 'tick-medium.png'
+    else 'tick-grey.png' end
+  end
 end
