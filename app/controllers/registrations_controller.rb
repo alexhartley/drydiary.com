f52ef1d868db9ab0cc11a17c983101603c9b64c1
Russell <russell@russelldunphy.com>
2013-06-22 20:11:18 +0100

    Fix registrations controller refactoring

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters, :only => [:create, :update]
+  before_filter :configure_permitted_parameters_for_create, only: :create
+  before_filter :configure_permitted_parameters_for_update, only: :update
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up)        { u.permit(*sign_up_params) }
-    devise_parameter_sanitizer.for(:account_update) { u.permit(*account_update_params) }
+  def configure_permitted_parameters_for_create
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(*sign_up_permitted_params)
+    end
+  end
+
+  def configure_permitted_parameters_for_update
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(*account_update_permitted_params)
+    end
   end
 
   private
 
-  def sign_up_params
+  def sign_up_permitted_params
     [
       :username,
       :email,
@@ -21,7 +29,7 @@ class RegistrationsController < Devise::RegistrationsController
     ]
   end
 
-  def account_update_params
+  def account_update_permitted_params
     [
       :username,
       :email,
