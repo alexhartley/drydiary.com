ebba477e19bac3fdb9ea9d879cefa499835a9e9b
Russell <russell@russelldunphy.com>
2013-06-24 16:56:19 +0100

    Set mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ DrydiaryCom::Application.configure do
     domain: 'drydiary.com',
   }
 
+  config.action_mailer.default_url_options = { :host => 'drydiary.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
