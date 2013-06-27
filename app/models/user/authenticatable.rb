c2e21675b13ff96cf2418889cdd48a07fdaa8c19
Russell <russell@russelldunphy.com>
2013-06-27 12:55:38 +0100

    Remove attempt at modularising user

--- app/models/user/authenticatable.rb
+++
@@ -1,7 +0,0 @@
-class User
-  module Authenticatable
-    include Mongoid::Document
-    field :email,              type: String, default: ""
-    field :encrypted_password, type: String, default: ""
-  end
-end
