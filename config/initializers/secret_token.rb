9c2155e6f615b1c1e916f9021645b25fd834f7fb
Russell <russell@russelldunphy.com>
2013-07-01 19:12:13 +0100

    Default token for development

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DrydiaryCom::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+DrydiaryCom::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || "DEVELOPMENT TOKEN NOT USED IN TEH PRODUCTION"
