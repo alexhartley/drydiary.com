c6929519238b1ad1900c86a450156691dfb3818c
Russell <russell@russelldunphy.com>
2013-07-01 19:08:39 +0100

    Use secret token from config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DrydiaryCom::Application.config.secret_key_base = '39c78dc768b0b12c7e6610ef388d45d39b3f31b05d4ba88c840b0aa74e3e864870dd637b6cd80cd580b3106065ba2b9b0d1e4d9515323f0f6bc639da3394131e'
+DrydiaryCom::Application.config.secret_key_base = ENV['SECRET_TOKEN']
